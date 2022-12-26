1a4bb631de4a4c8b63a095b62512a54a9e30e2b7
Bad983 <daniele.badiali83@gmail.com>
2022-12-26 22:45:07 +0100

    Review project

--- translate_inferno.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-model_checkpoint = "Helsinki-NLP/opus-mt-en-it"
-translator = pipeline("translation", model=model_checkpoint)
-
-trad = translator("How are you?")
-print(trad[0]['translation_text'])
